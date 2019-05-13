5ac7a0d01fb6209d2510e7d3f332f3eda774a179
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2019-05-13 18:41:48 +0300

    Improve session loading for Tensorflow

--- nlpservice/nlp/utils.py
+++ nlpservice/nlp/utils.py
@@ -1,30 +1,47 @@
 import tensorflow as tf
 import tensorflow_hub as hub
 
-MODELS = {
-    'UniversalSentenceEncoder':
-    'https://tfhub.dev/google/universal-sentence-encoder-large/3'
-    # 'https://tfhub.dev/google/universal-sentence-encoder/3',
-}
+SESSIONS = {}
 
 
 def get_model(model, settings=None):
-    path = MODELS[model]
+    if model not in SESSIONS:
+        path, loader = MODELS[model]
+        SESSIONS[model] = loader(path)
 
-    return hub.Module(path)
+    return SESSIONS[model]
+
+
+def load_use(path):
+    # tf.device('/cpu:0')     # :)
+    g = tf.Graph()
+
+    with g.as_default():
+        placeholder = tf.placeholder(dtype=tf.string, shape=[None])
+        embed = hub.Module(path)
+        op = embed(placeholder)
+        init = [tf.global_variables_initializer(), tf.tables_initializer()]
+        init_op = tf.group(init)
+
+    g.finalize()
+    session = tf.Session(graph=g)
+    session.run(init_op)
+
+    return session, op, placeholder
+
+
+MODELS = {
+    'UniversalSentenceEncoder':
+    ('https://tfhub.dev/google/universal-sentence-encoder-large/3', load_use),
+    # 'https://tfhub.dev/google/universal-sentence-encoder/3',
+}
 
 
 def sentences2vec(sentences, model=None):
     if model is None:
         model = 'UniversalSentenceEncoder'
 
-    # tf.device('/cpu:0')     # :)
-    model = get_model(model)
-    init = [tf.global_variables_initializer(), tf.tables_initializer()]
-
-    with tf.Session() as session:
-        session.run(init)
-        embed = model(sentences)
-        vectors = session.run(embed)
+    sess, op, placeholder = get_model(model)
+    vectors = sess.run(op, feed_dict={placeholder: sentences})
 
     return vectors
--- nlpservice/nlpservice-ui/src/components/Summarize.vue
+++ nlpservice/nlpservice-ui/src/components/Summarize.vue
@@ -1,19 +1,45 @@
 <template>
-  <v-container>
-    <v-layout text-xs-center wrap>
+  <v-container grid-list-md>
+    <v-layout row wrap>
 
-      <v-flex mb-5 xs12>
-        <h2 class="headline font-weight-bold mb-3">Summarize</h2>
-        <v-sheet color="#DDD" elevation="1" min-height="10em">
-          {{ summary }}
+      <v-flex xs12 md6 item>
+        <v-sheet theme="light">
+          <h2 class="headline font-weight-bold mb-3">Summary</h2>
+          <v-sheet color="#DDD" elevation="1" min-height="10em">
+            {{ summary }}
+          </v-sheet>
+          <v-btn>Copy to clibboard</v-btn>
         </v-sheet>
+      </v-flex>
 
-        <v-form>
-          <v-textarea v-model="text"></v-textarea>
-          <v-btn @click="submit">Submit</v-btn>
-        </v-form>
+      <v-flex xs12 md6 item>
+        <v-container grid-list-md>
+          <v-layout column wrap>
+            <v-flex>
+              <v-card tile flat color="amber lighten-2">
+                <v-card-text>
+                  <v-form>
+                    Enter text:
+                    <v-textarea v-model="text"></v-textarea>
+                  </v-form>
+                </v-card-text>
+                <v-card-actions>
+                  <v-btn flat @click="submit">Submit</v-btn>
+                </v-card-actions>
+              </v-card>
+            </v-flex>
+            <v-flex>
+              <v-card tile flat color="purple lighten-4">
+                <v-card-text>
+                  <div v-for="sent in sentences">
+                    {{ sent }}
+                  </div>
+                </v-card-text>
+              </v-card>
+            </v-flex>
+          </v-layout>
+        </v-container>
 
-        <v-layout justify-center></v-layout>
       </v-flex>
 
     </v-layout>
@@ -27,6 +53,7 @@
       return {
         'summary': '',
         'text': '',
+        'sentences': [],
       }
     },
     methods: {
@@ -34,7 +61,9 @@
         axios
           .post('http://localhost:6543/summarize', {'text': this.text})
           .then((resp) => {
-            this.summary = resp.data.summary.preview
+            let data = resp.data.summary
+            this.summary = data.preview
+            this.sentences = data.sentences
             console.log(resp)
           })
       }
