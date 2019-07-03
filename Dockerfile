30f1cdd03cd00a057c5c0111d93ae0489905de7b
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2019-07-03 22:33:09 +0300

    Improve dockerization

--- Dockerfile
+++ Dockerfile
@@ -1,24 +1,28 @@
 FROM tensorflow/tensorflow:latest-py3
 LABEL maintainer="Tiberiu Ichim <tiberiu.ichim@eaudeweb.ro>"
 
+RUN apt-get update \
+    && apt-get install -y --no-install-recommends \
+        gosu \
+        vim \
+    && rm -rf /var/lib/apt/lists/*
+
 RUN mkdir -p /data
 RUN useradd --system -m -d /data -U -u 500 nlp \
 	&& chown -R 500 /data
 
-COPY ./ /app
+COPY . /app
 
-RUN pip3 install -r /app/requirements.txt
+RUN pip3 install --no-cache-dir -r /app/requirements.txt
 
 RUN pip3 install -e /app
 
 RUN python -m nltk.downloader stopwords
 RUN python -m nltk.downloader punkt
 
-COPY docker/* /
+VOLUME /data/nlp
 
-VOLUME /data
-
-RUN /docker-setup.sh
+# RUN /app/docker/docker-setup.sh
 
 EXPOSE 6543
 WORKDIR /app
@@ -26,5 +30,7 @@ WORKDIR /app
 HEALTHCHECK --interval=1m --timeout=5s --start-period=1m \
   CMD nc -z -w5 127.0.0.1 6543 || exit 1
 
-ENTRYPOINT ["/docker-entrypoint.sh"]
-CMD ["start"]
+#ENTRYPOINT ["/app/docker/docker-entrypoint.sh"]
+
+#CMD ["start"]
+CMD ["bash"]
--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -4,6 +4,8 @@ set -e
 START="start"
 CMD="gunicorn --paster /app/setting.ini -b 0.0.0.0:6543"
 
+GOSU="gosu"
+
 # TODO: move these ops to a Makefile
 PREPARE="prepare --es-url=http://elasticsearch:9200/content /data/corpus.txt"
 LABEL="label /data/corpus.txt /data/labeled-corpus --kg-url=http://nginx/api/knowledge-graph/dump_all/ "
@@ -11,15 +13,15 @@ TRAIN_KV="kv /data/corpus.txt /data/corpus-ft"
 TRAIN_KERAS="train --cpu data/k-model data/corpus-ft data/corpus.txt --kg-url=http://nginx/api/knowledge-graph/dump_all/"
 TRAIN_FT="fasttext supervised -input ./data/labeled-corpus-train -output ./data/labeled-corpus -lr 0.5 -epoch 40 -wordNgrams 2 -bucket 2000000 -dim 50"
 
-if [ ! -e "/data/.models-built" ]; then
-  gosu nlp mkdir /data/model_cache
-  gosu nlp mkdir /data/cache
-  gosu nlp $PREPARE
-  gosu nlp $LABEL
-  gosu nlp $TRAIN_KV
-  gosu nlp $TRAIN_KERAS
-  gosu nlp $TRAIN_FT
-  gosu nlp touch /data/.models-built
+if [ ! -e "/data/nlp/.models-built" ]; then
+  $GOSU nlp mkdir /data/model_cache
+  $GOSU nlp mkdir /data/cache
+  $GOSU nlp $PREPARE
+  $GOSU nlp $LABEL
+  $GOSU nlp $TRAIN_KV
+  $GOSU nlp $TRAIN_KERAS
+  $GOSU nlp $TRAIN_FT
+  $GOSU nlp touch /data/nlp/.models-built
 fi
 
 if [[ $START == *"$1"* ]]; then
